acf5282d2dd811c094f2fff1a88ecb5bc463c200
Christian Plitz <christian.plitz@tum.de>
2022-12-04 13:25:25 -0300

    package added

---
+++ build/precache-manifest.a9c57c7d8a5cf5a16af5ff29d9fa8ef9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "832a740db3c53c71d650b5550f7ad21b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4616bc8b2d159f244653",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b21af18f12df680ab143",
+    "url": "/static/css/main.e5173af5.chunk.css"
+  },
+  {
+    "revision": "4616bc8b2d159f244653",
+    "url": "/static/js/2.3f3308d5.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.3f3308d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21af18f12df680ab143",
+    "url": "/static/js/main.6e064278.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
